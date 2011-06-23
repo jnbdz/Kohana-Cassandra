ae94ad5841ab8969728ea927b464666b12c7ffc2
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-06-23 12:02:53 -0400

    updated auth cassandra support

--- classes/kohana/auth/cassandra.php
+++ classes/kohana/auth/cassandra.php
@@ -23,37 +23,15 @@ class Kohana_Auth_Cassandra extends Auth {
 		if ( ! $user)
 			return FALSE;
 
-		if ($user instanceof Model_User AND $user->loaded())
-		{
-			// If we don't have a roll no further checking is needed
-			if ( ! $role)
-				return TRUE;
+		// Get all the roles
+		$columnFamily = CASSANDRA::selectColumnFamily('Users');
+		$userData = $columnFamily->get($username);
 
-			if (is_array($role))
-			{
-				// Get all the roles
-				$columnFamily = CASSANDRA::selectColumnFamily('Users');
-				$roles = $columnFamily->get('roles');
+		if (!$userData['role'])
+			return FALSE;
 
-				if (count($roles) !== count($role))
-					return FALSE;
+		return $userData['role'];
 
-			}
-			else
-			{
-				if ( ! is_object($role))
-				{
-					// Load the role
-					$roles = ORM::factory('role', array('name' => $role));
-
-					if ( ! $roles->loaded())
-						return FALSE;
-				}
-			}
-
-			return $user->has('roles', $roles);
-
-		}
 	}
 
 	/**
@@ -64,5 +42,214 @@ class Kohana_Auth_Cassandra extends Auth {
 	 * @param   boolean  enable autologin
 	 * @return  boolean
 	 */
-//	protected function _login($user, $password, $remember)
+	protected function _login($user, $password, $remember)
+	{
+
+		if ( ! is_object($user))
+		{
+			$username = $user;
+
+			// Load the user
+			$user = CASSANDRA::selectColumnFamily('Users')->get($username);
+		}
+
+		// If the passwords match, perform a login
+		if ($user['password'] === $password)
+		{
+			if ($remember === TRUE)
+			{
+				// Token data
+				$data = array(
+					'expires'    => time() + $this->_config['lifetime'],
+					'user_agent' => sha1(Request::$user_agent),
+				);
+
+				// Create a new autologin token
+				$token = CASSANDRA::selectColumnFamily('UsersTokens')->insert($username, $data);
+
+				// Set the autologin cookie
+				Cookie::set('authautologin', $token->token, $this->_config['lifetime']);
+			}
+
+			// Finish the login
+			$this->complete_login($user);
+
+			return TRUE;
+		}
+
+		// Login failed
+		return FALSE;
+	}
+
+	/**
+	 * Forces a user to be logged in, without specifying a password.
+	 *
+	 * @param   mixed    username string, or user ORM object
+	 * @param   boolean  mark the session as forced
+	 * @return  boolean
+	 */
+	public function force_login($user, $mark_session_as_forced = FALSE)
+	{
+
+		if ( ! is_object($user))
+		{
+			$username = $user;
+
+			// Load the user
+			$user = CASSANDRA::selectColumnFamily('Users')->get($username);
+		}
+
+		if ($mark_session_as_forced === TRUE)
+		{
+			// Mark the session as forced, to prevent users from changing account information
+			$this->_session->set('auth_forced', TRUE);
+		}
+
+		// Run the standard completion
+		$this->complete_login($user);
+
+	}
+
+	/**
+	 * Logs a user in, based on the authautologin cookie.
+	 *
+	 * @return  mixed
+	 */
+
+	public function auto_login()
+	{
+		if ($token = Cookie::get('authautologin'))
+		{
+			// Load the token and user
+			$token = CASSANDRA::selectColumnFamily('UsersTokens')->get($username, array('token' => $token)) 
+
+			if (is_array($token))
+			{
+				if ($token['user_agent'] === sha1(Request::$user_agent))
+				{
+					// Save the token to create a new unique token
+					CASSANDRA::selectColumnFamily('UsersTokens')->insert();
+
+					// Set the new token
+					Cookie::set('authautologin', $token->token, $token->expires - time());
+
+					// Complete the login with the found data
+					$user = CASSANDRA::selectColumnFamily('Users')->get($username);
+					$this->complete_login($user);
+
+					// Automatic login was successful
+					return $user;
+				}
+
+				// Token is invalid
+				CASSANDRA::selectColumnFamily('UsersTokens')->insert();
+			}
+		}
+
+		return FALSE;
+
+	}
+
+	/**
+         * Gets the currently logged in user from the session (with auto_login check).
+         * Returns FALSE if no user is currently logged in.
+         *
+         * @return  mixed
+         */
+        public function get_user($default = NULL)
+        {
+                $user = parent::get_user($default);
+
+                if ( ! $user)
+                {
+                        // check for "remembered" login
+                        $user = $this->auto_login();
+                }
+
+                return $user;
+        }
+
+        /**
+         * Log a user out and remove any autologin cookies.
+         *
+         * @param   boolean  completely destroy the session
+         * @param       boolean  remove all tokens for user
+         * @return  boolean
+         */
+        public function logout($destroy = FALSE, $logout_all = FALSE)
+        {
+                // Set by force_login()
+                $this->_session->delete('auth_forced');
+
+                if ($token = Cookie::get('authautologin'))
+                {
+                        // Delete the autologin cookie to prevent re-login
+                        Cookie::delete('authautologin');
+
+                        // Clear the autologin token from the database
+                        $token = ORM::factory('user_token', array('token' => $token));
+
+                        if ($token->loaded() AND $logout_all)
+                        {
+                                ORM::factory('user_token')->where('user_id', '=', $token->user_id)->delete_all();
+                        }
+                        elseif ($token->loaded())
+                        {
+                                $token->delete();
+                        }
+                }
+
+                return parent::logout($destroy);
+        }
+
+        /**
+         * Get the stored password for a username.
+         *
+         * @param   mixed   username string, or user ORM object
+         * @return  string
+	*/
+        public function password($user)
+        {
+                if ( ! is_object($user))
+                {
+                        $username = $user;
+
+                        // Load the user
+                        $user = ORM::factory('user');
+                        $user->where($user->unique_key($username), '=', $username)->find();
+                }
+
+                return $user->password;
+        }
+
+        /**
+         * Complete the login for a user by incrementing the logins and setting
+         * session data: user_id, username, roles.
+         *
+         * @param   object  user ORM object
+         * @return  void
+         */
+        protected function complete_login($user)
+        {
+                $user->complete_login();
+
+                return parent::complete_login($user);
+        }
+
+        /**
+         * Compare password with original (hashed). Works for current (logged in) user
+         *
+         * @param   string  $password
+         * @return  boolean
+         */
+        public function check_password($password)
+        {
+                $user = $this->get_user();
+
+                if ( ! $user)
+                        return FALSE;
+
+                return ($this->hash($password) === $user->password);
+        }
+
 }
