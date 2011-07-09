e5af5cc3fade8479f3e6f9ba7312e906c693f2a9
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-07-09 00:14:04 -0400

    ggg

--- classes/model/auth/user.php
+++ classes/model/auth/user.php
@@ -176,6 +176,7 @@ class Model_Auth_User {
 	public static function create_user($fields, $username)
 	{
 		self::Validate($fields);
+		die('Its here!');
 		CASSANDRA::selectColumnFamily('UsersRoles')->insert($username, array('rolename' => 'login'));
 		return CASSANDRA::selectColumnFamily('Users')->insert($username, array('email' => $fields['email'], 'password' => $fields['password']));
 	}
