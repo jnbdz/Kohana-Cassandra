5ab239631917ca97ebfd9b2875788494345e6ede
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-09-23 00:26:41 -0400

    ...

--- classes/kohana/auth/cassandra.php
+++ classes/kohana/auth/cassandra.php
@@ -59,7 +59,11 @@ class Kohana_Auth_Cassandra extends Auth {
 				if($i === 1)
 				{
 					$log = new Log;
-					$log->add(Log::ERROR, 'There was a conflic with the username and/or email. UUID: '.$user['uuid'].' username: '.$user['username'].' email: '.$user['email']);
+					$log->add(Log::ERROR, 'There was a conflic with the username and/or email. UUID: :uuid username: :username email: :uuid', array(
+						':email'        => $user['email'],
+						':username'     => $user['username'],
+						':uuid'         => $user['uuid'],
+					));
 					Message::add('error', __('There is a conflic. Please wait will we clear this problem.'));
 					$user = FALSE;
 					break;
