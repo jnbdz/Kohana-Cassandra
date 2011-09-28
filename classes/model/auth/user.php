a2a4ebbc544d24ad8c561453d118eaab707a6c16
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-09-28 14:15:37 -0400

    changed the rules

--- classes/model/auth/user.php
+++ classes/model/auth/user.php
@@ -29,24 +29,24 @@ class Model_Auth_User {
 	 */
 	protected $_rules = array(
 		'username' => array(
-			'not_empty' => NULL,
-			'min_length' => array(4),
-			'max_length' => array(32),
-			'regex' => array('/^[-\pL\pN_.]++$/uD'),	
+			array('not_empty'),
+			array('min_length' array(4)),
+			array('max_length', array(32)),
+			array('regex', array('/^[-\pL\pN_.]++$/uD')),
 		),
 		'password' => array(
-			'not_empty' => NULL,
-			'min_length' => array(8),
-			'max_length' => array(42),
+			array('not_empty'),
+			array('min_length', array(8)),
+			array('max_length', array(42)),
 		),
 		'password_confirm' => array(
-			'matches' => array('password'),
+			array('matches', array('password')),
 		),
 		'email' => array(
-			'not_empty' => NULL,
-			'min_length' => array(4),
-			'max_length' => array(127),
-			'email' => NULL,
+			array('not_empty'),
+			array('min_length', array(4)),
+			array('max_length', array(127)),
+			array('email'),
 		),
 	);
 
